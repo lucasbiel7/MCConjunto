1e49d9f0c34a87afc9bfd646178e974e5469b441
lucasbiel7 <lucasbiel7@icloud.com>
2017-11-08 21:19:34 -0200

    Finalizar aplicação ao fechar a tela principal

--- src/main/java/br/com/MCConjunto/model/Running.java
+++ src/main/java/br/com/MCConjunto/model/Running.java
@@ -6,7 +6,10 @@
 package br.com.MCConjunto.model;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
@@ -17,6 +20,10 @@ public class Running extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         GerenciadorDeJanela gerenciadorDeJanela=new GerenciadorDeJanela();
+        primaryStage.setOnCloseRequest((WindowEvent event) -> {
+            Platform.exit();
+            System.exit(0);
+        });
         gerenciadorDeJanela.mostrarJanela(primaryStage, gerenciadorDeJanela.carregarComponente("Menu"), "Início").show();
     }
     
