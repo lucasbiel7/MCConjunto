a568f770b49e544f0c8086fe5c11caf93ddc4401
lucasbiel7 <lucasbiel7@icloud.com>
2017-11-07 22:36:33 -0200

    Inicializando projeto

---
+++ src/main/java/br/com/MCConjunto/control/MenuController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.MCConjunto.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author lucas
+ */
+public class MenuController implements Initializable {
+
+    @FXML
+    private AnchorPane apPrincipal;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+            // TODO
+        
+    }    
+    
+}
