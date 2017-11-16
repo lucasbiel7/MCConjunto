f3378671481e992c7825e8209626b8bffa8d06e7
lucasbiel7 <lucasbiel7@icloud.com>
2017-11-15 22:19:48 -0200

    Adicionando botões ao menu de manipular conjuntos

---
+++ src/main/java/br/com/MCConjunto/control/ManipularConjuntoController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.MCConjunto.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author lucas
+ */
+public class ManipularConjuntoController implements Initializable {
+
+    
+    @FXML
+    private AnchorPane apPrincipal;
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void onActionCompararConjunto(ActionEvent ae){
+        
+    }
+    
+    @FXML
+    private void onActionConjuntoPotencia(ActionEvent ae){
+    
+    }
+
+    @FXML
+    private void onActionPertenceAoConjunto(ActionEvent ae){
+    
+    }
+    
+    @FXML
+    private void onActionConjuntoUniao(ActionEvent ae){
+    
+    }
+    
+    @FXML
+    private void onActionConjuntoIntersecao(ActionEvent ae){
+    
+    }
+    
+    @FXML
+    private void onActionConjuntoDiferenca(ActionEvent ae){
+    
+    }
+    
+    @FXML
+    private void onProdutoCartesiano(ActionEvent ae){
+        
+    }
+}
