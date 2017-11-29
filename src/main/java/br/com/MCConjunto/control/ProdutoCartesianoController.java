f09d6afed7ee244f4ca4f12d551b5b6c93229720
Paulo <phenriquesantos17@gmail.com.br>
2017-11-29 01:54:39 -0200

    Criação dos campos para selecionar os conjuntos e ao selocionar os conjuntos apresentar os elementos deles.

--- src/main/java/br/com/MCConjunto/control/ProdutoCartesianoController.java
+++ src/main/java/br/com/MCConjunto/control/ProdutoCartesianoController.java
@@ -6,12 +6,17 @@
 package br.com.MCConjunto.control;
 
 import br.com.MCConjunto.model.Conjunto;
+import br.com.MCConjunto.model.ManipularConjunto;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -23,8 +28,12 @@ public class ProdutoCartesianoController implements Initializable {
 
     @FXML
     private AnchorPane apPrincipal;
-    
+    @FXML
+    private ComboBox<Conjunto<Character>> cbPrimeiroConjunto;
+    @FXML
+    private Label lbPrimeiroConjunto;
     private List<Conjunto<Character>> conjuntos;
+    private ManipularConjunto manipularConjunto;
     
     /**
      * Initializes the controller class.
@@ -34,8 +43,15 @@ public class ProdutoCartesianoController implements Initializable {
         Platform.runLater(()->{
             if(apPrincipal.getUserData()!=null){
                 conjuntos=(List<Conjunto<Character>>) apPrincipal.getUserData();
+                cbPrimeiroConjunto.getItems().setAll(conjuntos);
+
             }
         });
-    }    
+        manipularConjunto=new ManipularConjunto();
+    }
+    @FXML
+    public void onActionPrimeiroConjunto (ActionEvent ae) {
+lbPrimeiroConjunto.setText(manipularConjunto.apresentarConjunto(cbPrimeiroConjunto.getSelectionModel().getSelectedItem()));
+    } 
     
 }
